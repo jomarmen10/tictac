a096136c4ef58f152b9c522ad7cc64ed8a517f90
JmendozaWin10 <mendoza.jomar910@gmail.com>
2022-06-16 02:44:37 -0700

    Add the basic variables and trying to make the event listener work

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// Variables
+
 const PlayerX_Won = "Player X won";
 const PlayerY_Won = "Player Y won";
 const Tie = "Game is a Tie";
+const isGameActive = true;
+
 const winningCondition = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,7 +14,7 @@ const winningCondition = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+];
 
 // board index 
 // [0][1][2]
@@ -18,3 +22,13 @@ const winningCondition = [
 // [6][7][8]
 
 let board = ["", "", "", "", "", "", "", "", ""];
+
+// Functions
+
+let cells = document.getElementById("grid");
+
+document.addEventListener("click", clickHandler);
+
+function clickHandler(e) {
+    console.log(e.target.innerText)
+}
